54a3faf695b4bee07052637ae7b71c83b5091a6a
nikosnaoum <47526732+nikosnaoum@users.noreply.github.com>
2020-01-03 13:38:04 +0200

    Initial commit

---
+++ app/deployment/recommendations-app.py
@@ -0,0 +1,74 @@
+"""Main module for App Engine app."""
+
+from flask import Flask, jsonify, request, render_template
+from recommendations import Recommendations
+import json
+
+app = Flask(__name__)
+rec_util = Recommendations()
+DEFAULT_RECS = 5
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/recommendation', methods=['GET'])
+def recommendation():
+    """Given a user id, return a list of recommended movie ids."""
+    user_id = request.args.get('UserId')
+    num_recs = request.args.get('NumRec')
+
+    # validate args
+    if user_id is None:
+        return 'No User Id provided.', 400
+    if num_recs is None:
+        num_recs = DEFAULT_RECS
+    try:
+        uid_int = int(user_id)
+        nrecs_int = int(num_recs)
+    except:
+        return 'User id and number of recs arguments must be integers.', 400
+
+    # Get recommended movies
+    rec_list = rec_util.get_recommendations(uid_int, nrecs_int)
+
+    if rec_list is None:
+        return 'User Id not found : %s' % user_id, 400
+
+#     json_response = jsonify({'movies': [str(i) for i in rec_list]})
+    return render_template("index.html", movies=rec_list)
+
+
+
+@app.route('/prediction', methods=['GET'])
+def prediction():
+    """Given a user id and a movie id, return a rating """
+    user_id = request.args.get('UserId')
+    movie_id = request.args.get('MovieId')
+
+    # validate args
+    if user_id is None:
+        return 'No User Id provided.', 400
+    if movie_id is None:
+        return 'No Movie Id provided.', 400
+    try:
+        uid_int = int(user_id)
+        mid_int = int(movie_id)
+    except:
+        return 'User id and movie id arguments must be integers.', 400
+
+    # Get predicted rating
+    rating, actual_rating = rec_util.get_prediction(uid_int, mid_int)
+
+    if rating is None:
+        return 'User Id or Movie Id not found : %s' % user_id, 400
+
+    return render_template("index.html", rating=rating, actual_rating=actual_rating)
+
+
+@app.route('/readiness_check', methods=['GET'])
+def readiness_check():
+    return '', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
